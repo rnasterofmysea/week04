42255ed3a2b2fca3cc5767dde1237d02e7b4f7da
rnasterofmysea <rnasterofmysea@gamil.com>
2021-09-19 21:56:23 +0900

    version02

--- syntax04.go
+++ syntax04.go
@@ -1,30 +1,31 @@
-package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+// package main
 
-// 난수 추출된 수의 소수 판정 프로그램 v0.1
-// 소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수 (0과 1은 제외)
-func main() {
-	seed := time.Now().Unix()
-	rand.Seed(seed)
+// import (
+// 	"fmt"
+// 	"math/rand"
+// 	"time"
+// )
 
-	count := 0
-	number := rand.Intn(150) + 2 // 0과 1제외, 2 ~ 151 사이의 수
-	fmt.Println("임의로 추출된 수 : ", number)
+// // 난수 추출된 수의 소수 판정 프로그램 v0.2
+// func main() {
+// 	seed := time.Now().Unix()
+// 	rand.Seed(seed)
 
-	for i := 1; i <= number; i++ {
-		if number%i == 0 {
-			count++
-		}
-	}
+// 	count := 0
+// 	number := rand.Intn(150) + 2 // 0과 1제외, 2 ~ 151 사이의 수
+// 	fmt.Println("임의로 추출된 수 : ", number)
 
-	if count == 2 {
-		fmt.Println(number, "는(은) 소수입니다!")
-	} else {
-		fmt.Println(number, "는(은) 소수가 아닙니다~")
-	}
-}
+// 	for i := 2; i < number; i++ { // 1과 number일때 loop 돌지 않음
+// 		if number%i == 0 {
+// 			//count++
+// 			count = count + 1
+// 		}
+// 	}
+
+// 	if count == 0 {
+// 		fmt.Println(number, "는(은) 소수입니다!")
+// 	} else {
+// 		fmt.Println(number, "는(은) 소수가 아닙니다~")
+// 	}
+// }
