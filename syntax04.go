2f7a712cb215c964db8039721ddf534d4484ba85
rnasterofmysea <rnasterofmysea@gamil.com>
2021-09-19 21:58:22 +0900

    version03

--- syntax04.go
+++ syntax04.go
@@ -1,31 +1,30 @@
+package main
 
-// package main
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-// import (
-// 	"fmt"
-// 	"math/rand"
-// 	"time"
-// )
+// 난수 추출된 수의 소수 판정 프로그램 v0.3
+func main() {
+	seed := time.Now().Unix()
+	rand.Seed(seed)
 
-// // 난수 추출된 수의 소수 판정 프로그램 v0.2
-// func main() {
-// 	seed := time.Now().Unix()
-// 	rand.Seed(seed)
+	isPrime := true
+	number := rand.Intn(150) + 2
+	fmt.Println("임의로 추출된 수 : ", number)
 
-// 	count := 0
-// 	number := rand.Intn(150) + 2 // 0과 1제외, 2 ~ 151 사이의 수
-// 	fmt.Println("임의로 추출된 수 : ", number)
+	for i := 2; i < number; i++ {
+		if number%i == 0 {
+			isPrime = false
+			//count = count + 1
+		}
+	}
 
-// 	for i := 2; i < number; i++ { // 1과 number일때 loop 돌지 않음
-// 		if number%i == 0 {
-// 			//count++
-// 			count = count + 1
-// 		}
-// 	}
-
-// 	if count == 0 {
-// 		fmt.Println(number, "는(은) 소수입니다!")
-// 	} else {
-// 		fmt.Println(number, "는(은) 소수가 아닙니다~")
-// 	}
-// }
+	if isPrime == true {
+		fmt.Println(number, "는(은) 소수입니다!")
+	} else {
+		fmt.Println(number, "는(은) 소수가 아닙니다~")
+	}
+}
